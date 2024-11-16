7aaff2bf22cd228c0fff2dce656fd5ddd76cb5b4
Madhan-sakthivel <madhans15498@gmail.com>
2024-11-16 20:13:00 +0530

    image added

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dbConfig = require("./config/db.config.js");
-
 const { hash, compare } = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const multer = require('multer');
 
-
-
+const upload = multer({ storage: storage });
+const path = require('path');
+const fs = require('fs');
 const { Sequelize, Model, DataTypes, where } = require('sequelize');
-
 const app = express();
 const PORT = 3000;
-
-
-// Middleware
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 //app.use(errorHandler)
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = path.join(__dirname, '../uploads');
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const fileName = `${uniqueSuffix}-${file.originalname}`;
+    cb(null, fileName);
+  }
+});
 
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
@@ -43,7 +56,7 @@ User.init({
   email: DataTypes.STRING,
   password: DataTypes.STRING
 },
-{ sequelize, modelName: 'User'},
+  { sequelize, modelName: 'User' },
 );
 
 
@@ -59,32 +72,32 @@ Car.init({
 
 app.post('/register', async (req, res) => {
   try {
-      let {email, password} = req.body
-      await sequelize.sync();
-      let user = await User.create(req.body)
-      res.status(201).json({id : user.id, email : user.email})
+    let { email, password } = req.body
+    await sequelize.sync();
+    let user = await User.create(req.body)
+    res.status(201).json({ id: user.id, email: user.email })
   } catch (error) {
-      console.log(error.message)
+    console.log(error.message)
   }
 })
 
 app.post('/login', async (req, res, next) => {
   try {
-      let {email, password} = req.body
-      if(!email) throw {name : "Email is required"}
-      if(!password) throw {name : "Password is required"}
-      let user = await User.findOne({
-          where : {
-              email
-          }
-      })
-      if(!user) throw {name : "Invalid email/password"}
-      let valid = compare(password, user.password)
-      if(!valid) throw {name : "Invalid email/password"}
-      let access_token = jwt.sign({id : user.id}, "secret")
-      res.status(200).json({access_token})
+    let { email, password } = req.body
+    if (!email) throw { name: "Email is required" }
+    if (!password) throw { name: "Password is required" }
+    let user = await User.findOne({
+      where: {
+        email
+      }
+    })
+    if (!user) throw { name: "Invalid email/password" }
+    let valid = compare(password, user.password)
+    if (!valid) throw { name: "Invalid email/password" }
+    let access_token = jwt.sign({ id: user.id }, "secret")
+    res.status(200).json({ access_token })
   } catch (error) {
-      next(error)
+    next(error)
   }
 })
 
@@ -92,7 +105,7 @@ app.post('/login', async (req, res, next) => {
 
 
 
-app.post('/car',authentication, (req, res) => {
+app.post('/car', authentication, (req, res) => {
   (async () => {
     await sequelize.sync();
     await Car.create(req.body).then(data => {
@@ -110,92 +123,92 @@ app.get('/car', (req, res) => {
 
 });
 
-app.get('/car/search', authentication,(req, res) => {
+app.get('/car/search', authentication, (req, res) => {
   (async () => {
     await sequelize.sync();
-    res.send(await Car.findAll({where:{name:req.query.name}}));
+    res.send(await Car.findAll({ where: { name: req.query.name } }));
   })();
 
 });
-  app.put('/car',authentication, (req, res) => {
-    (async () => {
-      await sequelize.sync();
-      await Car.update(req.body, {where: {id:req.body.id}}).then(data => {
-        res.send(data); 
-      });
-    })();
-  });
+app.put('/car', authentication, (req, res) => {
+  (async () => {
+    await sequelize.sync();
+    await Car.update(req.body, { where: { id: req.body.id } }).then(data => {
+      res.send(data);
+    });
+  })();
+});
 
-    app.delete('/car',authentication, (req, res) => {
-      const carId = req.query.id;
-      (async () => {
-        await sequelize.sync();
-        const result = await Car.destroy({where: {id: carId}})
+app.delete('/car', authentication, (req, res) => {
+  const carId = req.query.id;
+  (async () => {
+    await sequelize.sync();
+    const result = await Car.destroy({ where: { id: carId } })
 
-        if (result === 1) {
-          res.status(200).send({ message: 'Car deleted successfully' });
-        } else {
-          res.status(404).send({ message: 'Car not found' });
-        }
+    if (result === 1) {
+      res.status(200).send({ message: 'Car deleted successfully' });
+    } else {
+      res.status(404).send({ message: 'Car not found' });
+    }
 
-      })();
+  })();
 
 });
 
 
-function errorHandler(err, req, res, next){
+function errorHandler(err, req, res, next) {
   let status = err.status || 500
   let message = err.message || "Internal server error"
 
-  switch(err.name){
-      case "SequelizeValidationError":
-      case "SequelizeUniqueConstraintError":
-          status = 400;
-          message = err.errors[0].message
-          break;
-      case "Invalid Input":
-      case "Email is required":
-      case "Password is required":
-          status = 400;
-          message = err.name
-          break;
-      case "Invalid email/password":
-          status = 401;
-          message = err.name
-          break
-      case "Invalid token":
-      case "JsonWebTokenError":
-          status = 401;
-          message = "Invalid token"
-          break
-      case "Forbidden" :
-          status = 403;
-          message = "You are not authorized"
-          break;
-      case "Data not found":
-      case "Hero not found":
-          status = 404;
-          message = err.name
-          break;
+  switch (err.name) {
+    case "SequelizeValidationError":
+    case "SequelizeUniqueConstraintError":
+      status = 400;
+      message = err.errors[0].message
+      break;
+    case "Invalid Input":
+    case "Email is required":
+    case "Password is required":
+      status = 400;
+      message = err.name
+      break;
+    case "Invalid email/password":
+      status = 401;
+      message = err.name
+      break
+    case "Invalid token":
+    case "JsonWebTokenError":
+      status = 401;
+      message = "Invalid token"
+      break
+    case "Forbidden":
+      status = 403;
+      message = "You are not authorized"
+      break;
+    case "Data not found":
+    case "Hero not found":
+      status = 404;
+      message = err.name
+      break;
   }
-  res.status(status).json({message})
+  res.status(status).json({ message })
 }
 
-async function authentication(req, res, next){
+async function authentication(req, res, next) {
   try {
-      if(!req.headers.authorization) throw {name : "Invalid token"}
-      let [type, token] = req.headers.authorization.split(" ")
-      if(type !== "Bearer") throw {name : "Invalid token"}
-      let payload =  jwt.verify(token, "secret")
-      if(!payload) throw {name : "Invalid token"}
-      let user = await User.findByPk(payload.id)
-      if(!user) throw {name : "Invalid token"}
-      req.user = {
-          id : user.id
-      }
-      next()
+    if (!req.headers.authorization) throw { name: "Invalid token" }
+    let [type, token] = req.headers.authorization.split(" ")
+    if (type !== "Bearer") throw { name: "Invalid token" }
+    let payload = jwt.verify(token, "secret")
+    if (!payload) throw { name: "Invalid token" }
+    let user = await User.findByPk(payload.id)
+    if (!user) throw { name: "Invalid token" }
+    req.user = {
+      id: user.id
+    }
+    next()
   } catch (error) {
-      next(error)
+    next(error)
   }
 }
 
@@ -208,5 +221,3 @@ app.listen(PORT, (error) => {
     console.log("Error occurred, server can't start", error);
 }
 );
-
-
